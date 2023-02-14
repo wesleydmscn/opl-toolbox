409e4ac1c835611df2e5e711d11cc401a0075042
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-02-14 08:21:07 -0300

    refactor: format code to PEP 8 and python-practices

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 import os
+import time
+
+print('''
+
+ __ _     __    _  _  _  __
+/  / \|\||_    |_||_)|_)(_ 
+\__\_/| ||  ___| ||  |  __)
+ __ __    __ _  _ ___ _  _ 
+/__|_ |\||_ |_)|_| | / \|_)
+\_||__| ||__| \| | | \_/| \ 
+
+''')
+
+ROOT_DIR = 'APPS/'
+CONF_FILE = 'conf_apps.cfg'
+
 
 def init_script():
-  root_dir = os.listdir()
+    if os.path.isdir(ROOT_DIR):
+        find_elf_files()
+    else:
+        print('APPS/ folder not found.')
 
-  if 'APPS' in root_dir:
-    find_elf_files()
-  else:
-    print('404, APPS folder not found.')
 
 def find_elf_files():
-  files = os.listdir('APPS/')
-  match_list = []
+    files = os.listdir(ROOT_DIR)
+    elf_files = [f for f in files if f.endswith('.ELF')]
 
-  for elf_file in files:
-    if '.ELF' in elf_file:
-      match_list.append(elf_file)
+    if not elf_files:
+        print('.elf files not found.')
+        return
 
-  if len(match_list) == 0:
-    print('.elf files not found.')
-  else:
-    print(match_list)
-    create_conf_apps(match_list)
+    print(60 * '-')
+    print(f'The following .elf were found:\n {elf_files}')
+    print(60 * '-')
+
+    create_conf_apps(elf_files)
+
 
 def create_conf_apps(elf_list):
-  user_inputs = []
+    time.sleep(1)
+    user_inputs = []
 
-  for elf in elf_list:
-    user_input = str(input(f'Which name should appear in the APPS list for this elf: {elf}? '))
-    user_inputs.append([user_input, elf])
-  
-  cfg = open('conf_apps.cfg', 'w')
+    for elf in elf_list:
+        user_input = str(input(f'Name for this elf file ({elf}): '))
+        user_inputs.append([user_input, elf])
 
-  for path in user_inputs:
-    cfg.write(f'{path[0]}=mass:/APPS/{path[1]}\n')
-    
-  cfg.close()
+    try:
+        with open(CONF_FILE, 'w') as cfg:
+            for path in user_inputs:
+                cfg.write(f'{path[0]}=mass:/{ROOT_DIR}{path[1]}\n')
+
+            print(60 * '-')
+            print(f'\n{CONF_FILE} was created\n')
+            print(60 * '-')
+
+    except Exception as e:
+        print(f'An error occured while writing the file: {e}')
+
 
 init_script()
