4be1c202d53c275b732dc2863dc11263d2d8fbb0
Wesley Damasceno <wesleydmscn.co@gmail.com>
2024-06-03 10:45:41 -0300

    refactor: Migrate to JavaScript

--- .github/CONTRIBUTING.md
+++
@@ -1,9 +0,0 @@
-# Contributing
-
-Thanks for your interest in contributing to conf_apps-opl! Please take a moment to review this document **before submitting a pull request**.
-
-## Pull requests
-
-**Please ask first before starting work on any significant new features.**
-
-It's never a fun experience to have your pull request declined after investing a lot of time and effort into a new feature. To avoid this from happening, we request that contributors create [a feature request](https://github.com/wesleydmscn/conf_apps-opl/discussions/new?category=ideas) to first discuss any significant new ideas. This includes things like adding new utilities, creating new at-rules, etc.
---
+++ src/main.js
@@ -0,0 +1,81 @@
+/* MIT License
+ *
+ * Copyright (c) 2024 Wesley Damasceno
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk?version=4.0';
+import Adw from 'gi://Adw?version=1';
+
+import { OplToolboxWindow } from './window.js';
+
+pkg.initGettext();
+pkg.initFormat();
+
+export const OplToolboxApplication = GObject.registerClass(
+    class OplToolboxApplication extends Adw.Application {
+        constructor() {
+            super({application_id: 'org.wesleydmscn.opltoolbox', flags: Gio.ApplicationFlags.DEFAULT_FLAGS});
+
+            const quit_action = new Gio.SimpleAction({name: 'quit'});
+                quit_action.connect('activate', action => {
+                this.quit();
+            });
+            this.add_action(quit_action);
+            this.set_accels_for_action('app.quit', ['<primary>q']);
+
+            const show_about_action = new Gio.SimpleAction({name: 'about'});
+            show_about_action.connect('activate', action => {
+                let aboutParams = {
+                    transient_for: this.active_window,
+                    application_name: 'opl-toolbox',
+                    application_icon: 'org.wesleydmscn.opltoolbox',
+                    developer_name: 'Wesley Damasceno',
+                    version: '0.1.0',
+                    developers: [
+                        'Wesley Damasceno'
+                    ],
+                    copyright: '© 2024 Wesley Damasceno'
+                };
+                const aboutWindow = new Adw.AboutWindow(aboutParams);
+                aboutWindow.present();
+            });
+            this.add_action(show_about_action);
+        }
+
+        vfunc_activate() {
+            let {active_window} = this;
+
+            if (!active_window)
+                active_window = new OplToolboxWindow(this);
+
+            active_window.present();
+        }
+    }
+);
+
+export function main(argv) {
+    const application = new OplToolboxApplication();
+    return application.runAsync(argv);
+}
